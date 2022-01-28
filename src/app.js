834fec8214c42252d64b95dbfc5b7f42deabe868
botdotcom <shamli95singh@gmail.com>
2022-01-27 19:31:58 -0600

    Added templates, static and 404 support

--- src/app.js
+++ src/app.js
@@ -1,42 +1,84 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 const geocode = require('./service/geocode')
 const forecast = require('./service/forecast')
 
 const app = express()
 
+// directory paths
+const publicDirPath = path.join(__dirname, '../public')
+const viewsDirPath = path.join(__dirname, '../templates/views')
+const partialsDirPath = path.join(__dirname, '../templates/partials')
+
+// setup static/assets
+app.use(express.static(publicDirPath))
+
+// setup template engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsDirPath)
+
+// setup path for header and footer
+hbs.registerPartials(partialsDirPath)
+
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello Express.js!')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Shamli Singh'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Hello Express.js! This is the weather page')
+    // API requests with callback chaining
+    geocode('Newark', (error, {latitude, longitude, location}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: location
+            })
+        })
+    })
 })
 
 app.get('/help', (req, res) => {
-    res.send('Hello Express.js! This is the help page')
+    res.render('help', {
+        title: 'Weather App',
+        name: 'Shamli Singh'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('Hello Express.js! This is the about page')
+    res.render('about', {
+        title: 'Weather App',
+        name: 'Shamli Singh'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Shamli Singh',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Shamli Singh',
+        errorMessage: 'Page not found'
+    })
 })
 
 app.listen(3000, () => {
     console.log('Server is up and running...')
 })
-
-// API requests with callback chaining
-// geocode('Newark', (error, {latitude, longitude, location}) => {
-//     if (error) {
-//         return console.log(chalk.red.inverse('Cannot complete geolocation request! Please try again later...'))
-//     }
-
-//     forecast(latitude, longitude, (error, forecastData) => {
-//         if (error) {
-//             return console.log(chalk.red.inverse('Cannot complete weather request! Please try again later...'))
-//         }
-
-//         console.log(chalk.green(location))
-//         console.log(forecastData)
-//     })
-// })
