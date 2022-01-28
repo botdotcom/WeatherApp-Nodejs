4eae5a7a68cd42a126941025afb13a31716e440e
botdotcom <shamli95singh@gmail.com>
2022-01-27 22:27:56 -0600

    Changed port for compatibility

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./service/geocode')
 const forecast = require('./service/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // directory paths
 const publicDirPath = path.join(__dirname, '../public')
@@ -84,6 +85,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up and running...')
+app.listen(port, () => {
+    console.log('Server is up and running on port ' + port)
 })
